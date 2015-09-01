1f294d72feaf48960f5373bb19065aeb0d79ce7a
Vinicius Bisognin Immich <vinicius@Viniciuss-MacBook-Pro.local>
2015-09-01 11:19:29 -0300

    Added game var to popout.js

--- popup.js
+++ popup.js
@@ -1,13 +1,6 @@
-/*
- * $.getJSON("https://api.twitch.tv/kraken/games/top/?callback=?", function(data){
-	for(i=0; i < (data.top).length; i++){
-		$('body').append("<br>"+data.top[i].game.name);
-	}
-	console.log(data);
-});
-*/
+var game = 'Magic:%20The%20Gathering'
 
-$.getJSON("https://api.twitch.tv/kraken/streams?game=Magic:%20The%20Gathering", function(data){
+$.getJSON("https://api.twitch.tv/kraken/streams?game="+game, function(data){
 	for(i=0; i < (data.streams).length; i++){
 		if(typeof data.streams[i].channel.status != "undefined"){
 			originalTitle = data.streams[i].channel.status;
@@ -15,10 +8,9 @@ $.getJSON("https://api.twitch.tv/kraken/streams?game=Magic:%20The%20Gathering", 
 			originalTitle = data.streams[i].channel.name + " - Title Not Found";
 		}
 
-		title = originalTitle;	
+		title = originalTitle;
 		if(title.length > 40)
 			title = title.slice(0,40)+"...";
 		$('#streams').append("<tr><td><a href='"+data.streams[i].channel.url+"/popout/' target='blank' title='"+originalTitle+"'>"+title+"</a><br><b>   Channel: </b>"+data.streams[i].channel.name+"</td><td align='right'>"+data.streams[i].viewers+"</td></tr>");
 	}
-	console.log($('#streams').width());
 });
